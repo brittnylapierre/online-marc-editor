939cc12c84306a9704195a29df950484180dc84a
Tiago Murakami <trmurakami@gmail.com>
2022-02-03 08:41:02 -0300

    Reload

--- index.php
+++ index.php
@@ -126,14 +126,15 @@
     <main class="col-md-10 ms-sm-auto col-lg-11 px-md-4">
       <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
         <h1 class="h2">Editor</h1>
-        <!--
         <div class="btn-toolbar mb-2 mb-md-0">
           <div class="btn-group me-2">
+            <button type="button" class="btn btn-sm btn-outline-secondary" @click="cleanAll()">Clear all</button>
+            <!--
             <button type="button" class="btn btn-sm btn-outline-secondary">Share</button>
             <button type="button" class="btn btn-sm btn-outline-secondary">Export</button>
+            -->
           </div>
         </div>
-        -->
       </div>
       <div class="row">
           <div class="col-md-7 ms-sm-auto col-lg-8 px-md-4">
@@ -2326,6 +2327,9 @@
                         default:
                         this.record[field].push({ a: ""});
                     }
+                },
+                cleanAll () {
+                    document.location.reload(true);
                 },
                 copyTestingCode () {
                     try {
