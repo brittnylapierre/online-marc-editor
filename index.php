565d0fc9b5c65fb6a0dd2b470fd3f9081c0029e1
Tiago Murakami <trmurakami@gmail.com>
2022-02-08 09:36:43 -0300

    245

--- index.php
+++ index.php
@@ -1964,15 +1964,18 @@
 
 
                         <!-- TITLE -->
-                        <div class="input-group mb-3">
-                            <span class="input-group-text" id="title">Title&nbsp;&nbsp;
-                                <a href="https://www.loc.gov/marc/bibliographic/bd245.html" rel="external" target="_blank">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-circle" viewBox="0 0 16 16">
-                                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-                                    <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
-                                </svg>
-                                </a>
-                            </span>
+                        <div class="row mb-3">
+                            <div class="col-2">
+                                <span class="input-group-text" id="title">Title statement&nbsp;&nbsp;
+                                    <a href="https://www.loc.gov/marc/bibliographic/bd245.html" rel="external" target="_blank">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-circle" viewBox="0 0 16 16">
+                                        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                                        <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
+                                    </svg>
+                                    </a>
+                                </span>
+                            </div>
+                            <div class="col-10">
                             <div class="input-group-prepend">
                                 <select class="input-group-text form-select" id="_245_ind1" v-model="record._245_ind1">
                                     <option disabled>Title added entry</option>
@@ -1993,20 +1996,28 @@
                                     <option value="7">7</option>
                                 </select>
                             </div>
-                            <input type="text" id="title" v-model="record.title" class="form-control" placeholder="Title statement" aria-label="Title statement" aria-describedby="title">
+                            <div class="input-group">
+                                <span class="input-group-text" id="title">Title</span>
+                                <input type="text" id="title" v-model="record.title" class="form-control" placeholder="Title" aria-label="Title" aria-describedby="title">
+                            </div>
+                            <div class="input-group">
+                                <span class="input-group-text" id="subtitle">Subtitle</span>
+                                <input type="text" id="subtitle" v-model="record.subtitle" class="form-control" placeholder="Subtitle statement" aria-label="Subtitle statement" aria-describedby="subtitle">
+                            </div>
+                            <div class="input-group">
+                                <span class="input-group-text" id="245c">Statement of responsibility, etc.</span>
+                                <input type="text" id="245c" v-model="record._245c" class="form-control" placeholder="Statement of responsibility, etc." aria-label="Statement of responsibility, etc." aria-describedby="245c">
+                            </div>
+                            
+                            
+                            </div>
                         </div>
                         <!-- \TITLE -->
                         <!-- SUBTITLE -->
-                        <div class="input-group mb-3">
-                            <span class="input-group-text" id="title">Subtitle</span>
-                            <input type="text" id="subtitle" v-model="record.subtitle" class="form-control" aria-label="Subtitle statement" aria-describedby="subtitle">
-                        </div>
+
                         <!-- \SUBTITLE -->
                         <!-- 245c -->
-                        <div class="input-group mb-3">
-                            <span class="input-group-text" id="title">Statement of responsibility, etc.</span>
-                            <input type="text" id="245c" v-model="record._245c" class="form-control" aria-label="Statement of responsibility, etc." aria-describedby="245c">
-                        </div>
+
                         <!-- \245c -->
 
                         <!-- 260 -->
